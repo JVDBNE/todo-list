d4d36876a1a50fb7f262d89936b86425d24f5d73
Jess van Dyk <jessica.vandyk@xplortechnologies.com>
2022-07-30 11:26:53 +1000

    added basic css styling and completed state

--- src/App.js
+++ src/App.js
@@ -1,25 +1,51 @@
-import logo from './logo.svg';
+import React, { useState } from "react";
 import './App.css';
+import TodoItem from "./components/TodoItem/TodoItem";
+import TodoForm from "./components/TodoForm/TodoForm";
 
 function App() {
+
+  const [todos, setTodos] = useState([
+    {
+    text: "Learn about React",
+    isCompleted: false,
+    },
+    {
+    text: "Meet friend for lunch",
+    isCompleted: false,
+    },
+    {
+    text: "Build really cool todo app",
+    isCompleted: false,
+    },
+  ]);
+
+  const addTodo = (text) => {
+    const newTodos = [...todos, { text }];
+    setTodos(newTodos);};
+
+  const completeTodo = (index) => {
+    const newTodos = [...todos];
+    newTodos[index].isCompleted = true;
+    setTodos(newTodos);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <div className="app">
+    <div className="todo-list">
+    <h1>My To Do List</h1>
+    {todos.map((todo, index) => (
+        <TodoItem todo={todo} key={index} index={index}
+    completeTodo={completeTodo} />)
+    )
+  }
+  <TodoForm addTodo={addTodo} />
+  </div>
+  </div>
   );
+  
 }
 
 export default App;
+
+
